fc7b369742aac09a2826eb376ef7852e23583b7a
josephurban94 <joseph.urban94@gmail.com>
2021-12-06 17:52:09 +0200

    Create util.js
    created util.js. Placeholders for search functionality

---
+++ app/scripts/util.js
@@ -0,0 +1,30 @@
+function basicSearch() {
+    // basic search funtion, seaches for matches against orderid, customerid. Called when no search parameter is given, or if given parameter is invalid
+    let seachinput = ""
+
+
+}
+
+function orderIdSearch () {
+
+}
+
+function customerIdSearch () {
+    
+}
+
+function addressSearch () {
+    
+}
+
+function deliveryDateSearch () {
+    
+}
+
+function repSalesPersonSearch () {
+    
+}
+
+function productCodeSearch () {
+    
+}
