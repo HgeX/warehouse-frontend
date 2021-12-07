9a86669c5bc108d4a58bc1705f6df2750ec69967
josephurban94 <joseph.urban94@gmail.com>
2021-12-07 22:46:06 +0200

    util.js implementation
    implemented regex in util.js to operate on user input given in dashboard.html

--- app/scripts/util.js
+++ app/scripts/util.js
@@ -1,19 +1,20 @@
 function searchHandler() { //determines what type of search to run and calls relevant function
 
-    let searchinput = ""; //fetch from user input in search bar
+    let searchinput = document.getElementById("userInput").value; //fetch from user input in search bar
+    console.log("search input is: " + searchinput); // debug
+
+    let myRegex = /\w+(?=:)/; // finds whole word behind first colon in order to determine search parameter
+    let regexArray = myRegex.exec(searchinput); //performs regex operation on searchinput string to load result into regexArray
+
+    console.log("Regex array is: " + regexArray); // debug
+    console.log("Search parameter is: " + regexArray[0]); // debug
+
+    let searchparameter = regexArray[0]; // copies first regex match from array to a string //TODO fix to be case insensitive 
     
-    //DEPRECATED searchinput = searchinput.toLowerCase(); 
-    //let searchparameter = searchinput.REGEX_HERE
-    //regex ([a-z]*[A-Z]*[a-z]*)|([A-Z]*[a-z]*)(?=:) should probably maybe work even with bad casing, but fails on multiple separated capitals in the middle of the parameter string
-    //otherwise [a-z]*(?=:) is fine
-    //actually \w+(?=:) is better, turns out writing regex at 4am is a bad idea. Should remove need to convert lowercase
-    //let searchcontent = searchinput;
-    //let searchcontent = searchcontent.replace(REGEX_HERE);
-    //proposed regex for content \w+(?=:): => ""
-    // searchcontent should now be correct search string to search literally. Convert all to lower to make case insensitive??
 
-
-
+    let searchcontent = searchinput; // copies search input to a new string for manipulation
+    searchcontent = searchcontent.replace(/\w+(?=:):/, ""); // removes search parameter and colon from string, leaving just search contents
+    console.log("searchcontent is: " + searchcontent); // debug
 
     let switchmatchedflag = false;
 
@@ -21,36 +22,51 @@ function searchHandler() { //determines what type of search to run and calls rel
         case "orderid":
             orderIDSearch();
             switchmatchedflag = true;
+            console.log("orderid condition matched"); // debug
+
             break;
 
         case "customerid":
             customerIDSearch();
             switchmatchedflag = true;
+            console.log("customerid condition matched"); // debug
+            
             break;
 
         case "address":
             addressSearch();
             switchmatchedflag = true;
+            console.log("address condition matched"); // debug
+
             break;
 
         case "date":
             deliveryDateSearch();
             switchmatchedflag = true;
+            console.log("date condition matched"); // debug
+
             break;
 
         case "salesperson":
             repSalesPersonSearch();
             switchmatchedflag = true;
+            console.log("salesperson condition matched"); // debug
+
             break;
 
         case "productid":
             productCodeSearch();
             switchmatchedflag = true;
+            console.log("productid condition matched"); // debug
+
             break;
     }
 
     if (!switchmatchedflag) {
+        //TODO Basic search not called due to regex array = null
+        console.log("basic search condition matched");
         basicSearch();
+        
     }
 
 }
@@ -59,26 +75,26 @@ function basicSearch() { // basic search funtion, seaches for matches against or
     
 }
 
-function orderIDSearch() {
+function orderIDSearch() { // called with parameter "orderid"
 
 }
 
-function customerIDSearch() {
+function customerIDSearch() { // called with parameter "customerid"
     
 }
 
-function addressSearch() {
+function addressSearch() { // called with parameter "address"
     
 }
 
-function deliveryDateSearch() {
+function deliveryDateSearch() { // called with parameter "date"
     
 }
 
-function repSalesPersonSearch() {
+function repSalesPersonSearch() { // called with parameter "salesperson"
     
 }
 
-function productCodeSearch() {
+function productCodeSearch() { // called with parameter "productid"
     
 }
