d1dddfe479075c91d9112e076b7ff4cdc51d96a3
HgeX <atomi2500@gmail.com>
2021-12-08 16:29:02 +0200

    search bar drove me crazy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,11 +3,15 @@ const welcomeHook = document.getElementById('welcome');
 const hook = document.getElementById('hook');
 const searchInput = document.getElementById('search');
 const searchButton = document.getElementById('call-search');
+const clearSearchButton = document.getElementById('clear-icon');
 
-searchButton.addEventListener('click', event => {
-  event.preventDefault();
-  // TODO call searchHandler (util.js)
-});
+// searchButton.addEventListener('click', event => {
+//   event.preventDefault();
+//   // TODO call searchHandler (util.js)
+// });
 
 const formattedText = WELCOME_TEXT.concat(sessionStorage.getItem('username'));
 welcomeHook.textContent = formattedText;
+
+// Clear search input when the little x is clicked
+clearSearchButton.addEventListener('click', () => (searchInput.value = null));
