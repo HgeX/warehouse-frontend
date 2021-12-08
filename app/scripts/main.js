80600f088a7869d4ef702c664496dbe8b2c63cc8
HgeX <atomi2500@gmail.com>
2021-12-08 15:39:22 +0200

    Update main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,13 @@
 const WELCOME_TEXT = 'Welcome, ';
 const welcomeHook = document.getElementById('welcome');
 const hook = document.getElementById('hook');
+const searchInput = document.getElementById('search');
+const searchButton = document.getElementById('call-search');
+
+searchButton.addEventListener('click', event => {
+  event.preventDefault();
+  // TODO call searchHandler (util.js)
+});
 
 window.addEventListener('login', event => renderGreeting(event.user));
 
