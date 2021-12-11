4b60d8dea171722921b40ed4cbb20f05c9927feb
HgeX <atomi2500@gmail.com>
2021-12-11 18:52:22 +0200

    made search bar oh so much better (also somewhat responsive)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,26 +6,31 @@ const welcomeHook = document.getElementById('welcome');
 const hook = document.getElementById('hook');
 const searchInput = document.getElementById('search');
 const searchButton = document.getElementById('search-icon');
-const dummySearchButton = document.getElementById('dummy-search-button');
 const clearSearchButton = document.getElementById('clear-icon');
 const logoutButton = document.getElementById('logout-button');
 const loadingText = document.getElementById('loading-text');
 
-[searchButton, dummySearchButton].forEach(button =>
-  button.addEventListener('click', event => handleSearch(event))
-);
-
 const formattedText = WELCOME_TEXT.concat(sessionStorage.getItem('username'));
 welcomeHook.textContent = formattedText;
 
-// Clear search input when the little x is clicked
-clearSearchButton.addEventListener('click', () => (searchInput.value = null));
-// Redirect user to login screen, clear sessionStorage
+console.log(clearSearchButton);
+clearSearchButton.addEventListener('click', () => {
+  console.log('called');
+  searchInput.value = null;
+});
+
 logoutButton.addEventListener('click', event => {
   event.preventDefault();
   sessionStorage.removeItem('username');
   sessionStorage.removeItem(ORDERS_KEY);
   window.location.replace('./index.html');
+});
+
+searchButton.addEventListener('click', event => handleSearch(event));
+searchInput.addEventListener('keyup', event => {
+  if (searchInput === document.activeElement && event.code === 'Enter') {
+    handleSearch(event);
+  }
 });
 
 fetch(ORDERS_URL)
