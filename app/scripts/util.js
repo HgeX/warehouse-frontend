8ab2d58e7a82f21676ea14689b4af22d014a70a1
josephurban94 <joseph.urban94@gmail.com>
2021-12-14 00:59:05 +0200

    Cleanup and finalisation of util.js
    All search functions are fully implemented other than potential scope changes.
    All search functions now return an array of order objects.
    Some variables renamed for clarity.
    Debug messages removed.
    Most whitespace removed.
    General cleanup for readability.

--- app/scripts/util.js
+++ app/scripts/util.js
@@ -1,129 +1,111 @@
-export default function (searchinput, orders) {
-  //determines what type of search to run and calls relevant function
-
-  console.log(orders); // debug
+export default function (searchinput, orders) { // determines what type of search to run and calls relevant function. The function previously known as searchHandler()
+  //Pass a search string in the form "parameter:searchterm", and an array of order objects to search over. Parameters are not case-sensitive.
+  // accepts the following search parameters: 
+  // orderID - searches for orders with a matching orderID. Should only ever return 1 result.
+  // customerID - searches for orders with a matching customerID.
+  // address - searches for orders with a matching invaddr OR delivaddr. Case sensitive.
+  // date - searches for orders with a matching delivery date. Accepts ANY combination of the following date formats and separators: DD.MM.YY, D.M.YY, D/M/YYYY, D-MM-YYYY etc
+  // salesperson - searches for orders with matching respsaleperson. Not case sensitive
+  // productID - searches for orders which contain at least 1 instance of a product with the given ID/code. Not case sensitive
+  // If not given a search parameter, search will default to basic search, which searches the given term for matching customerID OR orderID values
 
   let output = [];
+  let parameterMatchRegex = /\w+(?=:)/; // finds whole word behind first colon in order to determine search parameter
+  let parameterArray = [];
+  let searchparameter = ""
+  let searchcontent = ""
 
-  console.log('search input is: ' + searchinput); // debug
-
-  let myRegex = /\w+(?=:)/; // finds whole word behind first colon in order to determine search parameter
-  let regexArray = [];
-
-  if (myRegex.exec(searchinput) != null) {
-    //terrible hack to prevent a null return
-    regexArray = myRegex.exec(searchinput); //performs regex operation on searchinput string to load result into regexArray
+  if (parameterMatchRegex.exec(searchinput) != null) { //terrible hack to prevent a null return
+    parameterArray = parameterMatchRegex.exec(searchinput); //performs regex operation on searchinput string to load result into parameterArray
   } else {
-    regexArray = ['dummy'];
+    parameterArray = ['dummy'];
   }
 
-  console.log('Regex array is: ' + regexArray); // debug
-  console.log('Search parameter is: ' + regexArray[0]); // debug
-
-  let searchparameter = regexArray[0].toLowerCase(); // copies first regex match from array to a string //TODO fix to be case insensitive
-
-  let searchcontent = searchinput; // copies search input to a new string for manipulation
+  searchparameter = parameterArray[0].toLowerCase(); // copies first regex match from array to a string
+  searchcontent = searchinput; // copies search input to a new string for manipulation
   searchcontent = searchcontent.replace(/\w+(?=:):/, ''); // removes search parameter and colon from string, leaving just search contents
-  console.log('searchcontent is: ' + searchcontent); // debug
 
-  switch (searchparameter) {
+  switch (searchparameter) { // matches search parameter to each case and calls relevant search function
     case 'orderid':
-      console.log('orderid condition matched'); // debug
       output = orderIDSearch(searchcontent, orders);
       break;
 
     case 'customerid':
-      console.log('customerid condition matched'); // debug
       output = customerIDSearch(searchcontent, orders);
       break;
 
     case 'address':
-      console.log('address condition matched'); // debug
       output = addressSearch(searchcontent, orders);
       break;
 
     case 'date':
-      console.log('date condition matched'); // debug
       output = deliveryDateSearch(searchcontent, orders);
       break;
 
     case 'salesperson':
-      console.log('salesperson condition matched'); // debug
       output = respSalesPersonSearch(searchcontent, orders);
       break;
 
     case 'productid':
-      console.log('productid condition matched'); // debug
       output = productCodeSearch(searchcontent, orders);
       break;
 
     default:
-      console.log('basic search condition matched'); // debug
       output = basicSearch(searchcontent, orders);
   }
-  console.log("Output of the searchHandler return is: " + output) //debug
   return output;
 }
 
-function basicSearch(searchTerm, ordersArray) { //TODO expand search scope
-  // basic search funtion, seaches for matches against orderid, customerid. Called when no search parameter is given, or if given parameter is invalid
+function basicSearch(searchTerm, ordersArray) { // basic search funtion, seaches for matches against orderid, customerid. Called when no search parameter is given, or if given parameter is invalid. eturns an array of order objects that match.
+  //TODO expand search scope?
   let results = [];
-
   for (let i=0; i < ordersArray.length; i++) {
     if ((ordersArray[i].orderid == searchTerm) || (ordersArray[i].customerid == searchTerm)) {
       results.push(ordersArray[i]);
-      console.log("Array of matched orders: " + results); //debug
     }
   }
   return results;
 }
 
-function orderIDSearch(searchTerm, ordersArray) {
-  // called with parameter "orderid". Returns an array of orderids that match
+function orderIDSearch(searchTerm, ordersArray) { // called with parameter "orderid". Returns an array of order objects that match.
   let results = [];
 
   for (let i=0; i < ordersArray.length; i++) {
     if (ordersArray[i].orderid == searchTerm) {
-      results.push(ordersArray[i].orderid);
-      console.log("Array of matched orders: " + results); //debug
+      results.push(ordersArray[i]);
     }
   }
   return results;
 }
 
-function customerIDSearch(searchTerm, ordersArray) {
-  // called with parameter "customerid". Returns an array of orderids that match
+function customerIDSearch(searchTerm, ordersArray) { // called with parameter "customerid". Returns an array of order objects that match.
   let results = [];
 
   for (let i=0; i < ordersArray.length; i++) {
     if (ordersArray[i].customerid == searchTerm) {
-      results.push(ordersArray[i].orderid);
-      console.log("Array of matched orders: " + results); //debug
+      results.push(ordersArray[i]);
     }
   }
   return results;
 }
 
-function addressSearch(searchTerm, ordersArray) { //TODO make case insensitive? I'm a bit worried that without doing it properly it'll cause unicode issues
-  // called with parameter "address". Returns an array of orderids that match
+function addressSearch(searchTerm, ordersArray) { // called with parameter "address". Returns an array of order objects that match.
+  //TODO make case insensitive? I'm a bit worried that without doing it properly it'll cause unicode issues
   let results = [];
 
   for (let i=0; i < ordersArray.length; i++) {
     if (ordersArray[i].invaddr == searchTerm) {
       results.push(ordersArray[i].orderid);
-      console.log("Array of matched orders: " + results); //debug
     } else if (ordersArray[i].delivaddr == searchTerm) {//I THINK that this elif should prevent duplicates when both delivery address and invoice address are filled and the same. Please leave as-is
-      results.push(ordersArray[i].orderid);
-      console.log("Array of matched orders: " + results);
+      results.push(ordersArray[i]);
     }
   }
   return results;
 }
 
-function deliveryDateSearch(searchTerm, ordersArray) {
-  // called with parameter "date". Returns an array of orderids that match
+function deliveryDateSearch(searchTerm, ordersArray) { // called with parameter "date". Returns an array of order objects that match.
   let results = [];
-  let dateRegex = /\d{1,4}/g; // finds DD, MM and YYYY (alt format D, M, YY) from searchTerm. Separators should be ignored
+  let dateRegex = /\d{1,4}/g; // finds DD, MM and YYYY (alt format D, M, YY) from searchTerm. Separators will be ignored
   let dateStorageArray = []; //TODO don't forget to catch a null result
   let searchString = "";
 
@@ -138,28 +120,24 @@ function deliveryDateSearch(searchTerm, ordersArray) {
     searchString = dateStorageArray[0] + "-" + dateStorageArray[1] + "-" + dateStorageArray[2];
     
     if (ordersArray[i].deliverydate == searchString) {
-      results.push(ordersArray[i].orderid);
-      console.log("Array of matched orders: " + results); //debug
+      results.push(ordersArray[i]);
     }
   }
   return results;
 }
 
-function respSalesPersonSearch(searchTerm, ordersArray) { 
-  // called with parameter "salesperson". Returns an array of orderids that match
+function respSalesPersonSearch(searchTerm, ordersArray) { // called with parameter "salesperson". Returns an array of order objects that match. 
   let results = [];
 
   for (let i=0; i < ordersArray.length; i++) {
     if (ordersArray[i].respsalesperson.toUpperCase() == searchTerm.toUpperCase()) { //TODO improve case desensitisation
-      results.push(ordersArray[i].orderid);
-      console.log("Array of matched orders: " + results); //debug
+      results.push(ordersArray[i]);
     }
   }
   return results;
 }
 
-function productCodeSearch(searchTerm, ordersArray) {
-  // called with parameter "productid". Returns an array of orderids that match
+function productCodeSearch(searchTerm, ordersArray) { // called with parameter "productid". Returns an array of order objects that match.
   let results = [];
   let productsArray = [];
 
@@ -167,8 +145,7 @@ function productCodeSearch(searchTerm, ordersArray) {
     productsArray = ordersArray[i].products;
     for (let j=0; j < productsArray.length; j++) {
       if (productsArray[j].code.toUpperCase() == searchTerm.toUpperCase()) { //TODO improve case desensitisation
-        results.push(ordersArray[i].orderid);
-        console.log("Array of matched orders: " + results); //debug
+        results.push(ordersArray[i]);
       }
     }
   }
