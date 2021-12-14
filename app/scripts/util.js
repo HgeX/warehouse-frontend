2867aa972ac387e6aa936db0d63929fabe2f44a1
HgeX <atomi2500@gmail.com>
2021-12-14 10:34:21 +0200

    implement is and not search criterias

--- app/scripts/util.js
+++ app/scripts/util.js
@@ -1,6 +1,7 @@
-export default function (searchinput, orders) { // determines what type of search to run and calls relevant function. The function previously known as searchHandler()
+export default function (searchinput, orders) {
+  // determines what type of search to run and calls relevant function. The function previously known as searchHandler()
   //Pass a search string in the form "parameter:searchterm", and an array of order objects to search over. Parameters are not case-sensitive.
-  // accepts the following search parameters: 
+  // accepts the following search parameters:
   // orderID - searches for orders with a matching orderID. Should only ever return 1 result.
   // customerID - searches for orders with a matching customerID.
   // address - searches for orders with a matching invaddr OR delivaddr. Case sensitive.
@@ -12,10 +13,11 @@ export default function (searchinput, orders) { // determines what type of searc
   let output = [];
   let parameterMatchRegex = /\w+(?=:)/; // finds whole word behind first colon in order to determine search parameter
   let parameterArray = [];
-  let searchparameter = ""
-  let searchcontent = ""
+  let searchparameter = '';
+  let searchcontent = '';
 
-  if (parameterMatchRegex.exec(searchinput) != null) { //terrible hack to prevent a null return
+  if (parameterMatchRegex.exec(searchinput) != null) {
+    //terrible hack to prevent a null return
     parameterArray = parameterMatchRegex.exec(searchinput); //performs regex operation on searchinput string to load result into parameterArray
   } else {
     parameterArray = ['dummy'];
@@ -25,7 +27,9 @@ export default function (searchinput, orders) { // determines what type of searc
   searchcontent = searchinput; // copies search input to a new string for manipulation
   searchcontent = searchcontent.replace(/\w+(?=:):/, ''); // removes search parameter and colon from string, leaving just search contents
 
-  switch (searchparameter) { // matches search parameter to each case and calls relevant search function
+  switch (
+    searchparameter // matches search parameter to each case and calls relevant search function
+  ) {
     case 'orderid':
       output = orderIDSearch(searchcontent, orders);
       break;
@@ -49,6 +53,12 @@ export default function (searchinput, orders) { // determines what type of searc
     case 'productid':
       output = productCodeSearch(searchcontent, orders);
       break;
+    case 'is':
+      output = isSearch(searchcontent, orders);
+      break;
+    case 'not':
+      output = isNotSearch(searchcontent, orders);
+      break;
 
     default:
       output = basicSearch(searchcontent, orders);
@@ -56,21 +66,26 @@ export default function (searchinput, orders) { // determines what type of searc
   return output;
 }
 
-function basicSearch(searchTerm, ordersArray) { // basic search funtion, seaches for matches against orderid, customerid. Called when no search parameter is given, or if given parameter is invalid. eturns an array of order objects that match.
+function basicSearch(searchTerm, ordersArray) {
+  // basic search funtion, seaches for matches against orderid, customerid. Called when no search parameter is given, or if given parameter is invalid. eturns an array of order objects that match.
   //TODO expand search scope?
   let results = [];
-  for (let i=0; i < ordersArray.length; i++) {
-    if ((ordersArray[i].orderid == searchTerm) || (ordersArray[i].customerid == searchTerm)) {
+  for (let i = 0; i < ordersArray.length; i++) {
+    if (
+      ordersArray[i].orderid == searchTerm ||
+      ordersArray[i].customerid == searchTerm
+    ) {
       results.push(ordersArray[i]);
     }
   }
   return results;
 }
 
-function orderIDSearch(searchTerm, ordersArray) { // called with parameter "orderid". Returns an array of order objects that match.
+function orderIDSearch(searchTerm, ordersArray) {
+  // called with parameter "orderid". Returns an array of order objects that match.
   let results = [];
 
-  for (let i=0; i < ordersArray.length; i++) {
+  for (let i = 0; i < ordersArray.length; i++) {
     if (ordersArray[i].orderid == searchTerm) {
       results.push(ordersArray[i]);
     }
@@ -78,10 +93,11 @@ function orderIDSearch(searchTerm, ordersArray) { // called with parameter "orde
   return results;
 }
 
-function customerIDSearch(searchTerm, ordersArray) { // called with parameter "customerid". Returns an array of order objects that match.
+function customerIDSearch(searchTerm, ordersArray) {
+  // called with parameter "customerid". Returns an array of order objects that match.
   let results = [];
 
-  for (let i=0; i < ordersArray.length; i++) {
+  for (let i = 0; i < ordersArray.length; i++) {
     if (ordersArray[i].customerid == searchTerm) {
       results.push(ordersArray[i]);
     }
@@ -89,27 +105,30 @@ function customerIDSearch(searchTerm, ordersArray) { // called with parameter "c
   return results;
 }
 
-function addressSearch(searchTerm, ordersArray) { // called with parameter "address". Returns an array of order objects that match.
+function addressSearch(searchTerm, ordersArray) {
+  // called with parameter "address". Returns an array of order objects that match.
   //TODO make case insensitive? I'm a bit worried that without doing it properly it'll cause unicode issues
   let results = [];
 
-  for (let i=0; i < ordersArray.length; i++) {
+  for (let i = 0; i < ordersArray.length; i++) {
     if (ordersArray[i].invaddr == searchTerm) {
       results.push(ordersArray[i]);
-    } else if (ordersArray[i].delivaddr == searchTerm) {//I THINK that this elif should prevent duplicates when both delivery address and invoice address are filled and the same. Please leave as-is
+    } else if (ordersArray[i].delivaddr == searchTerm) {
+      //I THINK that this elif should prevent duplicates when both delivery address and invoice address are filled and the same. Please leave as-is
       results.push(ordersArray[i]);
     }
   }
   return results;
 }
 
-function deliveryDateSearch(searchTerm, ordersArray) { // called with parameter "date". Returns an array of order objects that match.
+function deliveryDateSearch(searchTerm, ordersArray) {
+  // called with parameter "date". Returns an array of order objects that match.
   let results = [];
   let dateRegex = /\d{1,4}/g; // finds DD, MM and YYYY (alt format D, M, YY) from searchTerm. Separators will be ignored
   let dateStorageArray = []; //TODO don't forget to catch a null result
-  let searchString = "";
+  let searchString = '';
 
-  for (let i=0; i < ordersArray.length; i++) {
+  for (let i = 0; i < ordersArray.length; i++) {
     dateStorageArray = searchTerm.match(dateRegex);
 
     dateStorageArray[0] = parseInt(dateStorageArray[0]);
@@ -117,8 +136,13 @@ function deliveryDateSearch(searchTerm, ordersArray) { // called with parameter 
     if (dateStorageArray[2] < 99) {
       dateStorageArray[2] = parseInt(dateStorageArray[2]) + 2000;
     }
-    searchString = dateStorageArray[0] + "-" + dateStorageArray[1] + "-" + dateStorageArray[2];
-    
+    searchString =
+      dateStorageArray[0] +
+      '-' +
+      dateStorageArray[1] +
+      '-' +
+      dateStorageArray[2];
+
     if (ordersArray[i].deliverydate == searchString) {
       results.push(ordersArray[i]);
     }
@@ -126,28 +150,50 @@ function deliveryDateSearch(searchTerm, ordersArray) { // called with parameter 
   return results;
 }
 
-function respSalesPersonSearch(searchTerm, ordersArray) { // called with parameter "salesperson". Returns an array of order objects that match. 
+function respSalesPersonSearch(searchTerm, ordersArray) {
+  // called with parameter "salesperson". Returns an array of order objects that match.
   let results = [];
 
-  for (let i=0; i < ordersArray.length; i++) {
-    if (ordersArray[i].respsalesperson.toUpperCase() == searchTerm.toUpperCase()) { //TODO improve case desensitisation
+  for (let i = 0; i < ordersArray.length; i++) {
+    if (
+      ordersArray[i].respsalesperson.toUpperCase() == searchTerm.toUpperCase()
+    ) {
+      //TODO improve case desensitisation
       results.push(ordersArray[i]);
     }
   }
   return results;
 }
 
-function productCodeSearch(searchTerm, ordersArray) { // called with parameter "productid". Returns an array of order objects that match.
+function productCodeSearch(searchTerm, ordersArray) {
+  // called with parameter "productid". Returns an array of order objects that match.
   let results = [];
   let productsArray = [];
 
-  for (let i=0; i < ordersArray.length; i++) {
+  for (let i = 0; i < ordersArray.length; i++) {
     productsArray = ordersArray[i].products;
-    for (let j=0; j < productsArray.length; j++) {
-      if (productsArray[j].code.toUpperCase() == searchTerm.toUpperCase()) { //TODO improve case desensitisation
+    for (let j = 0; j < productsArray.length; j++) {
+      if (productsArray[j].code.toUpperCase() == searchTerm.toUpperCase()) {
+        //TODO improve case desensitisation
         results.push(ordersArray[i]);
       }
     }
   }
   return results;
+}
+
+function isSearch(searchTerm, ordersArray) {
+  if (searchTerm === 'ready' || searchTerm === 'done') {
+    return ordersArray.filter(each => each.ready);
+  }
+
+  return [];
+}
+
+function isNotSearch(searchTerm, ordersArray) {
+  if (searchTerm === 'ready' || searchTerm === 'done') {
+    return ordersArray.filter(each => !each.ready);
+  }
+
+  return [];
 }
